c9d4d98648b562af0bfaee18eb8570f17aa4983a
Steve Calla <callasteven@gmail.com>
2022-12-28 14:49:48 -0700

    Adjust for all errors

--- src/main/resources/static/js/delete-post.js
+++ src/main/resources/static/js/delete-post.js
@@ -1,22 +1,19 @@
 async function deleteFormHandler(event) {
   event.preventDefault();
 
-  document.location.replace('/dashboard')
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
 
-//  const id = window.location.toString().split('/')[
-//    window.location.toString().split('/').length - 1
-//  ];
-//
-//  const response = await fetch(`/api/posts/${id}`, {
-//    method: 'DELETE'
-//  });
-//
-//  if (response.ok) {
-//    document.location.replace('/dashboard')
-//  } else {
-////    document.location.replace('/dashboard')
-//    alert(response.statusText);
-//  }
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'DELETE'
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/')
+  } else {
+    alert(response.statusText);
+  }
 }
 
-//document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
